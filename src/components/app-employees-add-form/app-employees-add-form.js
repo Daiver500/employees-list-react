81a3a393c6c49e8b395f87951f0800bea6155ee8
Aleksei <60549770+Daiver500@users.noreply.github.com>
2022-01-19 15:54:47 +0300

    Update app-employees-add-form.js

--- src/components/app-employees-add-form/app-employees-add-form.js
+++ src/components/app-employees-add-form/app-employees-add-form.js
@@ -1,23 +1,48 @@
+import { Component } from "react";
 import "./app-employees-add-form.css"
 
-const EmployeesAddForm = () => {
-    return (
+class EmployeesAddForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: "",
+        salary: ""
+      }
+    }
+
+    onValueChange = (evt) => {
+      this.setState({
+        [evt.target.name]: evt.target.value,          // name, salary совпадают с версткой, а также пропсами, так мы можем достучаться до них
+      })
+    }
+
+    render () {
+        const {name,salary} = this.state
+        return (
         <div className="app-employees-add-form">
             <h3>Добавьте нового сотрудника</h3>
             <form
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+                    placeholder="Как его зовут?" 
+                    name="name"
+                    value={name}                               // чтобы реакт рендерил форму и контролирова ее поведение в ответ на действия пользователя, нужно добавлять атрибут value и в него добавлять значение state
+                    onChange={this.onValueChange}/>
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+                    placeholder="З/П в $?"
+                    name="salary"
+                    value={salary}
+                    onChange={this.onValueChange} />
 
                 <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
             </form>
         </div>
-    )
+        )
+    }
+    
 }
 
 export default EmployeesAddForm;
